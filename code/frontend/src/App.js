ac348847d3c84e425a1c43b994f536c20b9e079c
Ravindran Abilash <e18013@eng.pdn.ac.lk>
2022-11-27 15:11:36 +0530

    folder structure done
    created the folder structure
    add some dummy data
    created the setting button

--- code/frontend/src/App.js
+++ code/frontend/src/App.js
@@ -1,10 +1,25 @@
 import './App.css';
 
+import React, { useEffect } from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { FiSettings } from 'react-icons/fi';
+import { TooltipComponent } from '@syncfusion/ej2-react-popups';
+
 function App() {
   return (
-    <div className="App">
-      Test
-    </div>
+      <div >
+        <BrowserRouter>
+          <div className="flex relative dark:bg-main-dark-bg">
+            <div className="fixed right-4 bottom-4" style={{ zIndex: '1000' }}>
+              <TooltipComponent content="Settings" position="Top">
+                <button className="text-3xl text-white p-3 hover:drop-shadow-xl hover:bg-light-gray" style={{ background: "blueviolet", borderRadius: '50%' }}>
+                  <FiSettings />
+                </button >
+              </TooltipComponent>
+            </div>
+          </div>
+        </BrowserRouter>
+      </div>
   );
 }
 
