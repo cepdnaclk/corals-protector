1da131719be7fab032d4ad33df29461be709d733
Gowsigan A <e18115@eng.pdn.ac.lk>
2022-12-14 13:16:27 +0530

    Add Location Schema.

---
+++ code/backend/models/Location.js
@@ -0,0 +1,25 @@
+const mongoose = require("mongoose"); // imports mongoose
+const Schema = mongoose.Schema; // defines the strcuture of documents inside a collection
+
+const locationSchema = new Schema(
+  {
+    DatetTime: {
+      type: Date,
+      required: true,
+      default: Date.now,
+    },
+    longitude: {
+      type: int,
+      required: true,
+    },
+    latitude: {
+      type: int,
+      required: true,
+    },
+    // relationship with the Device (Has)
+    Pet: String, // FK | maps the Pet of the Location
+  },
+  { collection: "Location" }
+);
+
+module.exports = mongoose.model("LocationModel", locationSchema);
