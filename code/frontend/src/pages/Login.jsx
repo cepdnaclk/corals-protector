071f61da8fd1a570d03d9f88c7c6b01a2dcbf8ce
Ravindran Abilash <e18013@eng.pdn.ac.lk>
2022-12-27 10:53:53 +0530

    logout done
    logout done

--- code/frontend/src/pages/Login.jsx
+++ code/frontend/src/pages/Login.jsx
@@ -1,9 +1,5 @@
 import React, {useContext, useRef} from 'react'
 
-import { User } from '../data/data';
-
-
-import {useStateContext} from "../contexts/ContextProvider";
 import { loginCall } from "../apiCalls";
 import { AuthContext } from "../contexts/AuthContext";
 
@@ -19,7 +15,7 @@ const Login = () => {
             dispatch
         );
     };
-    console.log(user)
+
     return (
         <div className="flex min-h-full items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
             <div className="w-full max-w-md space-y-8">
