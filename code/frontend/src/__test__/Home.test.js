361a0f4c737274e359fcf7d94dad02744677baa3
Ravindran Abilash <e18013@eng.pdn.ac.lk>
2022-12-27 09:38:35 +0530

    Delete Home.test.js
    test file deleted from main branch

--- code/frontend/src/__test__/Home.test.js
+++
@@ -1,9 +0,0 @@
-import {Login} from "../pages";
-import {render, screen} from "@testing-library/react";
-
-test("get text", ()=>{
-    render(<Login/>);
-    const  textElement = screen.getByText(/Sign in to your account/i);
-    expect(textElement).toBeInDocument()
-    }
-);
